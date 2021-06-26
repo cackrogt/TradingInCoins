7bd101b2e0b3c6c4ddc6880099aa0417e4956ec2
Ankit Roy <cackrogt93@gmail.com>
2021-06-26 22:18:11 +0530

    Addition of table for the page and ngFor

--- TradingInCoins/src/app/main-page/main-page.component.ts
+++ TradingInCoins/src/app/main-page/main-page.component.ts
@@ -8,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class MainPageComponent implements OnInit {
 
   constructor() { }
-
+  public arr: any;
   ngOnInit(): void {
+    this.arr = [];
+    for (let i = 0; i < 10; i++){
+      let row: any= {
+        buyP : i,
+        currP : i + 1,
+        inc : 1,
+        lastIncTime : 2,
+        shouldSell : true
+      };
+      this.arr.push(row);
+    }
   }
 
 }
